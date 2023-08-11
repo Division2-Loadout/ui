544205459255a8ebd38ff71b4e9bbf23a4cbe949
Division2-Loadout <141663986+Division2-Loadout@users.noreply.github.com>
2023-08-11 17:49:58 +0100

    menu builds

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,9 @@
 db_menu = {
+    "builds": {
+        "title": "Builds",
+        "link": "#",
+        "target": "#"
+    },
     "help": {
         "title": "Help",
         "link": "https://github.com/Division2-Loadout/ui",
@@ -22,7 +27,12 @@ for (let v_key in db_menu) {
     v_link = db_menu[v_key]["link"]
     v_title = db_menu[v_key]["title"]
     v_target = db_menu[v_key]["target"]
-    content = `<a href="${v_link}" target="${v_target}">${v_title}</a> | `
+    if (v_key === "builds") {
+        content = `<a id="btn_search_build" href="#" title="Search" data-toggle="modal" data-target="#modal_search"><span class="glyphicon glyphicon-search" style="font-size: 12px"></span> ${v_title}</a> | `
+    } else {
+        content = `<a href="${v_link}" target="${v_target}">${v_title}</a> | `
+    }
+    
     $("#menu").append(content)
 }
 
