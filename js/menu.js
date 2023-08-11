3bf061bd20b144f0789ad1d2f4575a17b6c45128
Division2-Loadout <141663986+Division2-Loadout@users.noreply.github.com>
2023-08-11 17:37:01 +0100

    menu has been added

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,29 @@
 db_menu = {
-    "weapon": {
-        "title": "weapon",
-        "link": "weapon.html"
+    "help": {
+        "title": "Help",
+        "link": "https://github.com/Division2-Loadout/ui",
+        "target": "_blank"
     },
-    "gear": {
-        "title": "gear",
-        "link": "gear.html"
+    "credits": {
+        "title": "Credits",
+        "link": "https://github.com/Division2-Loadout/ui#credits-and-references",
+        "target": "_blank"
+    },
+    "disclaimer": {
+        "title": "Disclaimer",
+        "link": "https://github.com/Division2-Loadout/ui#disclaimer",
+        "target": "_blank"
     }
 }
+
+$("#menu").html(".> ")
 
 for (let v_key in db_menu) {
     v_link = db_menu[v_key]["link"]
     v_title = db_menu[v_key]["title"]
-    content = `<a href="${v_link}">${v_title}</a> | `
+    v_target = db_menu[v_key]["target"]
+    content = `<a href="${v_link}" target="${v_target}">${v_title}</a> | `
     $("#menu").append(content)
 }
+
+$("#menu").append(v_version)
